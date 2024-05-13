1f48a58a853ceca971664f33b03f55e2e953c334
DmitriyKvzarOlegovich <dkorolkovinfo@gmail.com>
2024-05-13 19:03:54 +0300

    Спринт 11 вариант1 19.05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,24 +36,30 @@ android {
 
     buildFeatures {
         dataBinding = true
-//Почему такая редакция дает ошибку при попытке синхронизации? (dataBinding true) хотя во всех источниках пишут именно такой пример
+
         viewBinding = true
     }
 
 }
 
 dependencies {
-
+//подключение библиотеки
+    implementation("com.google.code.gson:gson:2.10.1")
+    //подключение библиотеки ретрофит
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.7.2")
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
     //Подключение библиотеки Glide
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
     //
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.preference:preference:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,45 +1,54 @@
 package com.example.playlistmaker
 
 import android.content.Context
-
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-
-import android.widget.EditText
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 
 class SearchActivity : AppCompatActivity() {
     private var searchInputEditText: String? = AMOUNT_DEF
-
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderMessage2: TextView
+    private lateinit var placeholderImage: ImageView
+    private lateinit var buttonReBoot: Button
+    private  var searghtext:String =""
+    private var listTrack = ArrayList<TrackResponse.Track>()
+    private lateinit var rvTrack : RecyclerView
     companion object {
         private const val PRODUCT_AMOUNT = "inputEditText"
         private const val AMOUNT_DEF = ""
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        //setContentView(R.layout.activity_search)
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
-
-        val rvTrack = findViewById<RecyclerView>(R.id.trackListFace)
-        rvTrack.layoutManager = LinearLayoutManager(this)
-        val  listTrack: MutableList<Track> = ListTrack().filinglistTrack()
-        rvTrack.adapter = TrackAdapter(listTrack)
-
-
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        placeholderMessage2 = findViewById(R.id.placeholderMessage2)
+        placeholderImage = findViewById(R.id.placeholderImage)
+        buttonReBoot= findViewById(R.id.buttonReBoot)
+        buttonReBoot.visibility = View.GONE
+        rvTrack = findViewById<RecyclerView>(R.id.trackListFace)
 
 //нажатие на поиск Возврат в предидущее меню
         val buttonsearchBack = findViewById<TextView>(R.id.searchBack)
@@ -48,16 +57,24 @@ class SearchActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+//нажатие отработка повторного запроса плей листа
+        buttonReBoot.setOnClickListener {
+            listTrack = fillTrackList(searghtext)
+            rvTrack.adapter?.notifyDataSetChanged()
+            }
+
 //Работа с поиском
-        //val linearLayout = findViewById<LinearLayout>(R.id.containerList)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         clearButton.setOnClickListener {
             inputEditText.setText("")
             inputEditText.requestFocus()
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
             clearButton.isVisible = false
+            listTrack.clear()
+            rvTrack.adapter?.notifyDataSetChanged()
         }
         inputEditText.setText(searchInputEditText)
         inputEditText.requestFocus()
@@ -78,22 +95,122 @@ class SearchActivity : AppCompatActivity() {
                     clearButton.isVisible = true
                 }
             }
+
             override fun afterTextChanged(s: Editable?) {
                 onSaveInstanceState(Bundle())  // empty
             }
+
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+                //Сюда пришли если нажали кнопку доне
+
+                searghtext=inputEditText.text.toString()
+                listTrack = fillTrackList(searghtext)
+                rvTrack.layoutManager = LinearLayoutManager(this)
+                rvTrack.adapter = TrackAdapter(listTrack)
+                true
+            }
+            false
+        }
     }
 
-//Переопределяем сохранение зачения текстового поля ввода
+    //Переопределяем сохранение зачения текстового поля ввода
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
         savedInstanceState.putString(PRODUCT_AMOUNT, searchInputEditText)
     }
-//Переопределяем востановление зачения текстового поля ввода
+
+    //Переопределяем востановление зачения текстового поля ввода
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         searchInputEditText = savedInstanceState.getString(PRODUCT_AMOUNT)
+    }
+
+    fun fillTrackList(text: String?): ArrayList<TrackResponse.Track> {
+
+        val iTunesBaseUrl = "https://itunes.apple.com"
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl(iTunesBaseUrl).client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        var itunesService = retrofit.create(iTunesAPI::class.java)
+
+var temp: Call<TrackResponse> = itunesService.search(text)
+
+        temp.enqueue(object : Callback<TrackResponse> {
+            override fun onResponse(
+
+                call: Call<TrackResponse>,
+                response: Response<TrackResponse>
+            ) {
+                Log.d("TRANSLATION_LOG", "Status code: ${response.code()}")
+                if (response.code() == 200) { //полученн конкретный ответ
+                    listTrack.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        listTrack.addAll(response.body()?.results!!)
+                        rvTrack.adapter?.notifyDataSetChanged()
+                    }
+                    if (listTrack.isEmpty()) { //Если ответ получен но он пустой
+                        showMessage(getString(R.string.nothing_found), "")
+                    } else {
+                        showMessage("", "")
+                    }
+                } else { //ошибка сети ответа нет
+                    showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                 showMessage(getString(R.string.something_went_wrong), t.message.toString())
+            }
+
+        })
+
+        return listTrack
+    }
+
+    fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            //блок ничего не найдено
+            placeholderMessage.text=text
+            placeholderImage.setImageDrawable(getDrawable(R.drawable.not_found))
+            placeholderMessage.visibility = View.VISIBLE
+            placeholderMessage2.visibility = View.GONE
+            buttonReBoot.visibility = View.GONE
+            placeholderImage.visibility = View.VISIBLE
+            listTrack.clear()
+            rvTrack.adapter?.notifyDataSetChanged()
+
+            if (additionalMessage.isNotEmpty()) {
+                //блок проблем с интернетом
+                placeholderMessage2.text=getString(R.string.something_went_wrong)
+                placeholderImage.setImageDrawable(getDrawable(R.drawable.not_net))
+                placeholderMessage2.visibility = View.VISIBLE
+                placeholderMessage.visibility = View.GONE
+                placeholderImage.visibility = View.VISIBLE
+                buttonReBoot.visibility = View.VISIBLE
+                listTrack.clear()
+                rvTrack.adapter?.notifyDataSetChanged()
+
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            //блок  очистки поля для
+            placeholderMessage2.visibility = View.GONE
+            placeholderMessage.visibility = View.GONE
+            placeholderImage.visibility = View.GONE
+            buttonReBoot.visibility = View.GONE
+        }
     }
 
 }
